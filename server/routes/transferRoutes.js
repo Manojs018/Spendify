ffadfef2d8b4fbdeef1060a9aa3cf5b4311f3ac3
Manojs018 <>
2026-03-02 20:12:08 +0530

    Initial commit

--- server/routes/transferRoutes.js
+++ server/routes/transferRoutes.js
@@ -6,12 +6,14 @@ import {
 } from '../controllers/transferController.js';
 import { protect } from '../middleware/auth.js';
 
+import { cacheMiddleware } from '../middleware/cache.js';
+
 const router = express.Router();
 
 router.use(protect); // All routes are protected
 
 router.post('/send', sendMoney);
-router.get('/history', getTransferHistory);
+router.get('/history', cacheMiddleware('transfers'), getTransferHistory);
 router.get('/search', searchUsers);
 
 export default router;
